58655b1a3353e755713c7f2b9bdce0c3a88510dc
vncsmyrnk <vmayrink12@gmail.com>
2022-05-09 20:55:37 -0300

    Alterando algoritmo de busca por prioridade

--- src/Prioridade.java
+++ src/Prioridade.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.LinkedList;
 
 public class Prioridade {
@@ -18,53 +19,26 @@ public class Prioridade {
      * proximoFila = pedido;
      * }
      */
-
     public Pedido proximoPacote() {
         System.out.println("\nFila de pedidos: " + pedidos.toString());
         return this.menorPedidoPrioridade();
     }
 
     public Pedido menorPedidoTamanho() {
-        Pedido menor = this.pedidos.getFirst();
-        for (Pedido p : this.pedidos) {
-            if (p.quantidade < menor.quantidade) {
-                menor = p;
-            }
-        }
-        return this.pedidos.remove(this.pedidos.indexOf(menor));
-    }
+        Pedido menor = this.pedidos
+                .stream()
+                .min((p1, p2) -> p1.quantidade - p2.quantidade)
+                .get();
 
-    // 0 0 0 0 0 15 0
-
-    public Pedido menorPedidoPrioridade2() {
-        boolean isNotAll0 = true;
-        for (Pedido p : this.pedidos) {
-            if (p.prazo != 0) {
-                isNotAll0 = false;
-                continue;
-            }
-        }
-
-        if (!isNotAll0) {
-            return this.pedidos.removeFirst();
-        }
-
-        Pedido menor = this.pedidos.getFirst();
-        for (Pedido p : this.pedidos) {
-            if (p.prazo != 0 && p.prazo < menor.prazo) {
-                menor = p;
-            }
-        }
         return this.pedidos.remove(this.pedidos.indexOf(menor));
     }
 
     public Pedido menorPedidoPrioridade() {
-        Pedido menor = this.pedidos.getFirst();
-        for (Pedido p : this.pedidos) {
-            if (p.prazo != 0 && p.prazo < menor.prazo) {
-                menor = p;
-            }
-        }
+        Pedido menor = this.pedidos
+                .stream()
+                .min((p1, p2) -> Util.maxIntValueIfZero(p1.prazo) - Util.maxIntValueIfZero(p2.prazo))
+                .get();
+
         return this.pedidos.remove(this.pedidos.indexOf(menor));
     }
 
---
+++ src/Util.java
@@ -0,0 +1,5 @@
+public class Util {
+    public static int maxIntValueIfZero(int value) {
+        return value == 0 ? Integer.MAX_VALUE : value;
+    }
+}
